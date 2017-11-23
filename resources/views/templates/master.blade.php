09e11e777fad8de86336da52ceae83783ea9c7a1
Fabio Lopes <fzlopes1@gmail.com>
2017-11-23 20:08:27 -0200

    Criação do menu lateral.

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -1,11 +1,16 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
 	<head>
+	    <meta charset="utf-8">
 		<title>Investindo</title>
 		@yield('css-view')
 		<link rel="stylesheet" href="{{ asset('css/stylesheet.css')}}">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fredoka+One">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css">
 	</head>
 
     <body>
+        @include('templates.menu-lateral')
         @yield('conteudo-view')
         @yield('js-view')
     </body>
