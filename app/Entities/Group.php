24fde365cc003de449491dac0f1d60a79c7cd252
Fabio Lopes <fzlopes1@gmail.com>
2017-11-28 19:36:09 -0200

    Criação da entidade Group.

---
+++ app/Entities/Group.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class Group extends Model implements Transformable
+{
+    use softDeletes;
+    use TransformableTrait;
+
+    protected $fillable = ['name', 'user_id', 'institution_id'];
+
+    //Usuário lider do grupo
+    public function owner 
+    {
+    	return $this->belongsTo(User::class);
+    }
+
+    public functions institution
+    {
+    	return $this->belongsTo(Institution::class);
+    }
+}
