0ea5bd6125b8de8b480b6141d30aa9ede711b344
Fabio Lopes <fzlopes1@gmail.com>
2018-01-27 02:02:15 -0200

    Criação da entidade Moviment e de sua migration moviments.

---
+++ app/Entities/Moviment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class Moviment extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    protected $fillable = ['user_id', 'group_id', 'product_id', 'value', 'type'];
+
+}
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function group()
+	{
+		return $this->belongsTo(Group::class);
+	}
+
+	public function product()
+	{
+		return $this->belongstTo(Product::class);
+	}
